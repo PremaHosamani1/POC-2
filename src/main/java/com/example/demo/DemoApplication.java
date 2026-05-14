8cb37f3a74c656917a42d3bc6824f173b2bf984c
PremaHosamani1 <prema.hosamani2022@gmail.com>
2026-05-14 15:45:07 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "POC Working 🚀";
+        return "Jenkins Pipeline is working";
     }
 
     public static void main(String[] args) {
