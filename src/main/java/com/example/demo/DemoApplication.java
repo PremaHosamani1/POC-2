37891a88a94d3549aa6eaefec3e0b6a6092257e7
PremaHosamani1 <prema.hosamani2022@gmail.com>
2026-05-14 16:44:59 +0530

    Update home method return message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Jenkins Pipeline is working";
+        return "Jenkins Pipeline is working fine";
     }
 
     public static void main(String[] args) {
